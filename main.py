bbfa65b2506fd3b31121cf47c24bc642baab2b1a
TrongDungEmb98 <ntdungembedded98@gmail.com>
2022-12-27 23:09:54 +0700

    do login and enter

--- main.py
+++ main.py
@@ -4,53 +4,98 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import signal
+
+interrupted = False
+
+def signal_handler(signal, frame):
+    global interrupted
+    interrupted = True
+
+signal.signal(signal.SIGINT, signal_handler)
 
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-driver.get("https:/gmx.com")
 
 # close ads
-#.//*[@class='dialogOverlay']/div/div[1]/button
-# print("close ads")
-# try:
-#     wait = WebDriverWait(driver, 20)
-#     element = wait.until(EC.text_to_be_present_in_element(By.XPATH, ".//*[@class='dialogOverlay']/div/div[1]/button"))
-# except:
-#     print("not found text")
-# else:
-#     close_btn = driver.find_element(By.XPATH, ".//*[@class='dialogOverlay']/div/div[1]/button")
-#     close_btn.click()
+def close_ads():
+    print("close ads")
+    try:
+        # element = WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(By.CLASS_NAME, "js-shownomore"))
+        element = driver.find_element(By.CLASS_NAME, "js-shownomore")
+        element.click()
+    except:
+        print("not found text")
 
 # close accept policy
-print("accept policy")
-try:
-    element = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler")))
-    element.click()
-except:
-    print("not found text")
+def close_policy():
+    print("accept policy")
+    try:
+        # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler")))
+        element = driver.find_element(By.ID, "onetrust-accept-btn-handler")
+        element.click()
+    except:
+        print("not found text")
 
 # login button
-print("login")
-try:
-    element = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID, "login-button")))
-    element.click()
-except:
-    print("not found text")
+def click_login_btn():
+    print("login")
+    try:
+        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login-button")))
+        element.click()
+    except:
+        print("not found text")
+        return 0
+    else:
+        return 1
 
 # login-email
-print("fill user name")
-try:
-    element = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID, "login-email")))
-    element.send_keys("dungnt98_username")
-except:
-    print("not found text")
+def insert_username():
+    print("fill user name")
+    try:
+        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login-email")))
+        element.send_keys("dungnt98_username")
+    except:
+        print("not found text")
+        return 0
+    else:
+        return 1
 
 # login-password
-print("fill user name")
-try:
-    element = WebDriverWait(driver, 50).until(EC.element_to_be_clickable((By.ID, "login-password")))
-    element.send_keys("dungnt98_password")
-except:
-    print("not found text")
+def insert_password():
+    print("fill user name")
+    try:
+        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login-password")))
+        element.send_keys("dungnt98_password")
+    except:
+        print("not found text")
+        return 0
+    else:
+        return 1
 
-input("Press Enter to continue...")
+# do enter
+def do_enter():
+    try:
+        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login-password")))
+        element.send_keys(Keys.RETURN)
+    except:
+        print("not found text")
+        return 0
+    else:
+        return 1
+
+if __name__ == "__main__":
+    driver.get("https:/gmx.com")
+    while(True):
+        close_ads()
+        close_policy()
+        if (click_login_btn()):
+            ret = insert_username()
+            ret = insert_password()
+            if (ret):
+                do_enter()
+                input('wait key')
+
+        if interrupted:
+            quit()
+    
