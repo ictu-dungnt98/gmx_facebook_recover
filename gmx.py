5f757df6326cf26942adf4f864f07c28142f1f86
TrongDungEmb98 <ntdungembedded98@gmail.com>
2023-01-02 13:01:01 +0700

    login success, navigate to nav mail success

---
+++ gmx.py
@@ -0,0 +1,128 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+class Gmx:
+    def __init__(self, path = "C:\Program Files (x86)\chromedriver.exe"):
+        # self.driver
+        self.PATH = path
+        self.driver = webdriver.Chrome(self.PATH)
+    
+    # open web page
+    def open_url(self, url = "https:/gmx.com"):
+        self.driver.get(url)
+
+    # close ads
+    def close_ads(self):
+        print("close ads")
+        try:
+            # element = WebDriverWait(self.driver, 10).until(EC.text_to_be_present_in_element(By.CLASS_NAME, "js-shownomore"))
+            element = self.driver.find_element(By.CLASS_NAME, "js-shownomore")
+            element.click()
+        except:
+            print("not found text")
+
+    # close accept policy
+    def close_policy(self):
+        print("accept policy")
+        try:
+            # element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler")))
+            element = self.driver.find_element(By.ID, "onetrust-accept-btn-handler")
+            element.click()
+        except:
+            print("not found text")
+
+    def close_popup(self):
+        parent = self.driver.current_window_handle
+        uselessWindows = self.driver.window_handles
+        self.driver.switch_to.window(uselessWindows[-1])
+        self.driver.close()
+        self.driver.switch_to.window(uselessWindows[0])
+
+    # login button
+    def click_login_btn(self):
+        print("login")
+        try:
+            element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.ID, "login-button")))
+            element.click()
+        except:
+            print("not found text")
+            return 0
+        else:
+            return 1
+
+    # login-email
+    def insert_username(self, user):
+        print("fill user")
+        try:
+            element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.ID, "login-email")))
+            element.send_keys(user)
+        except:
+            print("insert_username not found text")
+            return 0
+        else:
+            return 1
+
+    # login-password
+    def insert_password(self, password):
+        print("fill password")
+        try:
+            element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.ID, "login-password")))
+            element.send_keys(password)
+        except:
+            print("insert_password not found text")
+            return 0
+        else:
+            return 1
+
+    # do enter
+    def do_enter(self):
+        try:
+            element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.ID, "login-password")))
+            element.send_keys(Keys.RETURN)
+        except:
+            print("do_enter not found text")
+            return 0
+        else:
+            return 1
+
+    # //*[name()='use' and @*='#nav_mail']
+    def click_nav_mail(self):
+        try:
+            # element = self.driver.find_element(By.XPATH, "//*[name()='use' and @*='#nav_mail']")
+            element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[name()='use' and @*='#nav_mail']")))
+            element.click()
+        except:
+            print("click_nav_mail not found text")
+            return 0
+        else:
+            return 1
+
+    def click_setting(self):
+        try:
+            # element = self.driver.find_element(By.XPATH, "//a[@id='navigationSettingsLink']")
+            # element.click()
+
+            # element = self.driver.find_element(By.ID, "navigationSettingsLink")
+            # element.click()
+
+            element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='navigationSettingsLink']")))
+            element.click()
+        except:
+            print("click_setting not found text")
+            return 0
+        else:
+            return 1
+
+    def click_alias_address(self):
+        try:
+            element = self.driver.find_element(By.ID, "id8e")
+            element.click()
+        except:
+            print("click_alias_address not found text")
+            return 0
+        else:
+            return 1
--- main.py
+++ main.py
@@ -1,101 +1,59 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from gmx import Gmx
 import time
 import signal
+import cv2
 
+# stop this program
 interrupted = False
-
 def signal_handler(signal, frame):
     global interrupted
     interrupted = True
-
 signal.signal(signal.SIGINT, signal_handler)
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
-
-# close ads
-def close_ads():
-    print("close ads")
-    try:
-        # element = WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(By.CLASS_NAME, "js-shownomore"))
-        element = driver.find_element(By.CLASS_NAME, "js-shownomore")
-        element.click()
-    except:
-        print("not found text")
-
-# close accept policy
-def close_policy():
-    print("accept policy")
-    try:
-        # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "onetrust-accept-btn-handler")))
-        element = driver.find_element(By.ID, "onetrust-accept-btn-handler")
-        element.click()
-    except:
-        print("not found text")
-
-# login button
-def click_login_btn():
-    print("login")
-    try:
-        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login-button")))
-        element.click()
-    except:
-        print("not found text")
-        return 0
-    else:
-        return 1
-
-# login-email
-def insert_username():
-    print("fill user name")
-    try:
-        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login-email")))
-        element.send_keys("dungnt98_username")
-    except:
-        print("not found text")
-        return 0
-    else:
-        return 1
-
-# login-password
-def insert_password():
-    print("fill user name")
-    try:
-        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login-password")))
-        element.send_keys("dungnt98_password")
-    except:
-        print("not found text")
-        return 0
-    else:
-        return 1
-
-# do enter
-def do_enter():
-    try:
-        element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "login-password")))
-        element.send_keys(Keys.RETURN)
-    except:
-        print("not found text")
-        return 0
-    else:
-        return 1
 
 if __name__ == "__main__":
-    driver.get("https:/gmx.com")
-    while(True):
-        close_ads()
-        close_policy()
-        if (click_login_btn()):
-            ret = insert_username()
-            ret = insert_password()
-            if (ret):
-                do_enter()
-                input('wait key')
 
+    # read file to get email
+    file = open("mail_live.txt", "r")
+    mails = file.readlines()
+    number_mails = len(mails)
+    using_mail = 0
+
+    # GMX start
+    gmx = Gmx()
+    gmx.open_url("https:/gmx.com")
+
+    #state machine
+    step = 0
+
+    while(True):
+        # gmx.close_ads()
+        # gmx.close_policy()
+        # gmx.close_popup()
+
+        if (step == 0):
+            user, password = mails[using_mail].split(":")
+            print(user)
+            print(password)
+            if (gmx.click_login_btn()):
+                ret = gmx.insert_username(user)
+                ret = gmx.insert_password(password)
+                if (ret):
+                    gmx.do_enter()
+                    step = 1
+        elif (step == 1):
+            if (gmx.click_nav_mail()):
+                step = 2
+        elif (step == 2):
+            if (gmx.click_setting()):
+                step = 3
+        elif (step == 3):
+            if (gmx.click_alias_address()):
+                step = 4
+        elif (step == 4):
+            print("step 4")
+
+        # check stop signal
         if interrupted:
             quit()
     
