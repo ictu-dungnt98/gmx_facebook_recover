0124af00245ec81ba8116ddbf31b3552ff83ca16
TrongDungEmb98 <ntdungembedded98@gmail.com>
2023-01-04 20:13:14 +0700

    fix click to alias address

--- gmx.py
+++ gmx.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
@@ -9,7 +10,11 @@ class Gmx:
     def __init__(self, path = "C:\Program Files (x86)\chromedriver.exe"):
         # self.driver
         self.PATH = path
-        self.driver = webdriver.Chrome(self.PATH)
+        self.options = Options()
+        self.options.add_argument('--ignore-certificate-errors')
+        self.options.add_argument('--ignore-ssl-errors')
+        self.options.add_argument('--disable-software-rasterizer')
+        self.driver = webdriver.Chrome(self.PATH, chrome_options=self.options)
         self.policy_pass = 0
         self.ads_close = 0
     
@@ -76,53 +81,28 @@ class Gmx:
 
     # login-password
     def insert_password(self, password):
-        try:
-            # element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.ID, "login-password")))
-            element = self.driver.find_element(By.ID, "login-password")
-            element.send_keys(password)
-        except:
-            print("insert_password not found text")
-            return 0
-        else:
-            return 1
+        # element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.ID, "login-password")))
+        element = self.driver.find_element(By.ID, "login-password")
+        element.send_keys(password)
+        print("insert_password not found text")
+        return 1
 
     def click_nav_mail(self):
-        try:
-            # element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[name()='use' and @*='#nav_mail']")))
-            element = self.driver.find_element(By.XPATH, "//*[name()='use' and @*='#nav_mail']")
-            element.click()
-            self.driver.implicitly_wait(2)
-        except:
-            print("click_nav_mail not found text")
-            return 0
-        else:
-            return 1
+        # element = WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[name()='use' and @*='#nav_mail']")))
+        element = self.driver.find_element(By.XPATH, "//*[name()='use' and @*='#nav_mail']")
+        element.click()
+        self.driver.implicitly_wait(5)
+        return 1
 
     def click_setting(self):
-        try:
-            iframe = self.driver.find_element(By.XPATH, "//*[@id=\"thirdPartyFrame_mail\"]")
-            self.driver.switch_to.frame(iframe)
-
-            element = self.driver.find_element(By.XPATH, "//a[@id=\"navigationSettingsLink\"]")
-            element.click()
-            self.driver.switch_to.default_content()
-            self.driver.implicitly_wait(2)
-        except:
-            return 0
-        else:
-            return 1
+        iframe = self.driver.find_element(By.XPATH, "//*[@id=\"thirdPartyFrame_mail\"]")
+        self.driver.switch_to.frame(iframe)
+        element = self.driver.find_element(By.XPATH, "//a[@id=\"navigationSettingsLink\"]")
+        element.click()
+        return 1
 
     def click_alias_address(self):
-        try:
-            self.driver.implicitly_wait(2)
-            iframe = self.driver.find_element(By.XPATH, "//*[@id=\"thirdPartyFrame_mail\"]")
-            self.driver.switch_to.frame(iframe)
-            element = self.driver.find_element(By.XPATH, "//*[@id=\"id8e\"]/div/div/div/ul/li[1]/div[2]/ul/li[5]/a")
-            element.click()
-        except:
-            print("click_alias_address not found text")
-            return 0
-        else:
-            self.driver.implicitly_wait(2)
-            self.driver.switch_to.default_content()
-            return 1
+        element = self.driver.find_element(By.XPATH, "//*[@id=\"id8e\"]/div/div/div/ul/li[1]/div[2]/ul/li[5]/a")
+        element.click()
+        self.driver.switch_to.default_content()
+        return 1
