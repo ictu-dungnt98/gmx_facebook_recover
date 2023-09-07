f8fada7b209b576899d73b66bf1e5498b2fad2b3
ictu-dungnt98 <ntdungembedded98@gmail.com>
2023-09-07 23:51:16 +0700

    click hien thi lop hoc

--- browser.py
+++ browser.py
@@ -10,7 +10,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 import re as regex
 
-from selenium.common.exceptions import UnexpectedAlertPresentException
+from selenium.common.exceptions import *
 
 class Browser:
     def __init__(self):
@@ -24,30 +24,6 @@ class Browser:
     def open_url(self, url = "google.com"):
         try:
             self.driver.get(url)
-        except:
-            return 0
-        else:
-            return 1
-
-    def check_page_working(self):
-        try:
-            element = self.driver.find_element(By.XPATH, "//*[@id='reload-button']")
-            element.click()
-            self.driver.implicitly_wait(2)
-        except:
-            return 0
-        else:
-            print("reload page by check_page_working")
-            self.refresh()
-            return 1
-
-    def check_page_working_2(self):
-        try:
-            element = self.driver.find_element(By.XPATH, "//*[@id='page-content']/main/div/div/div/div[1]/h1").text
-            print(element)
-            if (element.__contains__("We'll be back in a bit.")):
-                print("reload page by check_page_working_2")
-                self.refresh()
         except:
             return 0
         else:
@@ -81,50 +57,6 @@ class Browser:
         # Navigate to a webpage in the new tab
         self.driver.get(url)
         return 1
-
-    # close ads
-    def close_ads(self):
-        if (self.ads_close == 0):
-            try:
-                element = self.driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/a")
-                element.click()
-            except:
-                print("close_ads not found text")
-            else:
-                self.ads_close = 1
-
-    # close accept policy
-    def close_policy(self):
-        try:
-            iframe0 = self.driver.find_element(By.XPATH, "//*[@id=\"thirdPartyFrame_permission_dialog\"]")
-            if (iframe0.is_displayed()):
-                self.driver.switch_to.frame(iframe0)
-
-                try:
-                    self.driver.find_element(By.XPATH, "//*[@class='btn btn-secondary ghost']").click()
-                    self.driver.switch_to.default_content()
-                    return
-                except:
-                    pass
-
-                try:
-                    self.driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
-                    self.driver.switch_to.default_content()
-                    return
-                except:
-                    pass
-
-                self.driver.switch_to.frame(self.driver.find_element(By.XPATH, "//*[@id=\"permission-iframe\"]"))
-                try:
-                    self.driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
-                    self.driver.switch_to.default_content()
-                    return
-                except:
-                    pass
-
-                self.driver.switch_to.default_content()
-        except:
-            print("close_policy not found text")
 
     # login button
     def click_lets_go(self):
@@ -171,12 +103,31 @@ class Browser:
             element.send_keys(Keys.ENTER)
             return 1
         except UnexpectedAlertPresentException:
-            alert = self.driver.switch_to.alert
-            alert.accept()
-            return 1
+            try:
+                alert = self.driver.switch_to.alert
+                alert.accept()
+                return 1
+            except NoAlertPresentException:
+                return 1
         except:
             print("insert_password error")
             return 0
+    
+    def select_subject(self):
+        wait = WebDriverWait(self.driver, 3)
+        element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='drpCourse']")))
+
+        # chon mon hoc
+        # action = ActionChains(self.driver)
+        # action.click(on_element = element)
+        # action.perform()
+        select = Select(element)
+        select.select_by_visible_text("Dịch tễ học (2 TC)")
+
+        # click hien thi lop
+        # //*[@id="btnViewCourseClass"]
+        element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='btnViewCourseClass']")))
+        element.click()
 
     def context_insert_login_mail(self):
         print("context_insert_login_mail")
@@ -191,220 +142,3 @@ class Browser:
                 return 0
         except:
             return 0
-
-    def context_insert_login_pass(self):
-        print("context_insert_login_pass")
-        try:
-            wait = WebDriverWait(self.driver, self.time_check_context)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='right']/div/form/prism-text[2]")))
-            print(element.text)
-            if (element.text.__contains__("Enter your password to sign in securely")):
-                print("correct context")
-                return 1
-            else:
-                return 0
-        except:
-            return 0
-
-    def context_add_mail(self):
-        print("context_add_mail")
-        try:
-            wait = WebDriverWait(self.driver, self.time_check_context)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='page-content']/div/div/div/form/div[1]/h1")))
-            print(element.text)
-            if (element.text.__contains__("Update Your Xfinity Username")):
-                print("correct context")
-                return 1
-            else:
-                return 0
-        except:
-            return 0
-
-    # confirm-password
-    def insert_confirm_password(self, password):
-        try:
-            wait = WebDriverWait(self.driver, 3)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='oldPassword']")))
-            self.driver.execute_script("arguments[0].value = '';", element)
-            element.clear()
-            element.send_keys(password)
-            return 1
-        except:
-            print("fill confirm pass fail")
-            return 0
-
-    # new-user
-    def insert_new_user(self, user):
-        try:
-            wait = WebDriverWait(self.driver, 3)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='newUsername']")))
-            element.clear()
-            self.driver.execute_script("arguments[0].value = '';", element)
-            element.send_keys(user)
-            return 1
-        except:
-            print("fill new user fail")
-            return 0
-
-    # button save
-    def click_save(self):
-        print("click_save")
-        while(True):
-            try:
-                element = self.driver.find_element(By.XPATH, "//*[@id='page-content']/div/div/div/form/div[4]/div[1]/button")
-                element.click()
-            except:
-                print("click_save not found text")
-                return 0
-            else:
-                print("click_save success")
-                return 1
-
-    def back_to_login(self):
-        print("check_save_mail")
-        try:
-            wait = WebDriverWait(self.driver, 5)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='page-content']/div/div/div/a")))
-            element.click()
-            print("check_save_mail success")
-            return 1
-        except:
-            print("check_save_mail not found text")
-            return 0
-
-    def check_add_mail_success(self):
-        try:
-            wait = WebDriverWait(self.driver, 3)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='page-content']/div/div/div/h1")))
-            print(element.text)
-            if (element.text.__contains__("Your Xfinity username has been updated successfully.")):
-                print("Your Xfinity username has been updated successfully.")
-                return 1
-        except:
-            return 0
-
-    def check_add_mail_fail(self):
-        try:
-            wait = WebDriverWait(self.driver, 3)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='page-content']/div/div/div/form/p")))
-            print(element.text)
-            if (element.text.__contains__("This username is already taken")):
-                print("This username is already taken")
-                return 1
-        except:
-            return 0
-    
-    def check_same_old_user(self):
-        try:
-            wait = WebDriverWait(self.driver, 3)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='error_newUsername']")))
-            print(element.text)
-            if (element.text.__contains__("Your new username cannot be the same as your old username")):
-                print("Your new username cannot be the same as your old username")
-                return 1
-        except:
-            return 0
-
-    def check_pass_not_valid(self):
-        try:
-            wait = WebDriverWait(self.driver, 3)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='page-content']/div/div/div/form/p")))
-            print(element.text)
-            if (element.text.__contains__("Secret Question update failed. Password not valid")):
-                print("Secret Question update failed. Password not valid")
-                return 1
-        except:
-            return 0
-
-    def add_sub_mail(self, confirm_password, new_username):
-        if (self.insert_confirm_password(confirm_password) == 0):
-            return 0
-        
-        if (self.insert_new_user(new_username) == 0):
-            return 0
-        
-        if (self.click_save() == 0):
-            return 0
-
-        if (self.check_add_mail_success()):
-            print("add mail success: " + new_username)
-            return 1
-        elif (self.check_add_mail_fail()):
-            print("add mail fail")
-            return 2
-        elif (self.check_same_old_user()):
-            return 2
-        elif (self.check_pass_not_valid()):
-            return 2
-        else:
-            return 2
-
-    # //*[@id="identify_email"]
-    def fill_email_recover(self, email):
-        print("email_recover: " + email)
-        try:
-            wait = WebDriverWait(self.driver, 3)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='identify_email']")))
-            element.clear()
-            self.driver.execute_script("arguments[0].value = '';", element)
-            element.send_keys(email)
-        except:
-            print("fill_email_recover fail")
-            return 0
-        else:
-            return 1
-
-    #//*[@id="did_submit"]
-    def click_find(self):
-        print("click_find")
-        try:
-            wait = WebDriverWait(self.driver, 5)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='did_submit']")))
-            element.click()
-        except:
-            print("click_find not found text")
-            return 0
-        else:
-            print("click_find success")
-            return 1
-
-    #//*[@id="initiate_interstitial"]/div[3]/div/div[1]/button
-    def click_continue(self):
-        print("click_continue")
-        try:
-            wait = WebDriverWait(self.driver, 5)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='initiate_interstitial']/div[3]/div/div[1]/button")))
-            element.click()
-        except:
-            print("click_continue not found text")
-            return 0
-        else:
-            print("click_continue success")
-            return 1
-    
-    # //*[@id="identify_yourself_flow"]/div/div[2]/div[1]/div[2]
-    def check_search_email(self):
-        try:
-            wait = WebDriverWait(self.driver, 5)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='identify_yourself_flow']/div/div[2]/div[1]/div[2]")))
-            print(element.text)
-            if (element.text.__contains__("Your search did not return any results")):
-                print("Your search did not return any results")
-        except:
-            return 0
-        else:
-            return 1
-
-    # //*[@id="loginform"]/div[4]/a
-    def check_try_another_way(self):
-        print("check_try_another_way")
-        try:
-            wait = WebDriverWait(self.driver, 5)  # Maximum wait time of 3 seconds
-            element = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id='loginform']/div[4]/a")))
-            element.click()
-        except:
-            print("check_try_another_way not found text")
-            return 0
-        else:
-            print("check_try_another_way success")
-            return 1
--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import os
 
 STEP_OPEN_URL = 0
 STEP_READ_EMAIL_LOGIN = STEP_OPEN_URL + 1
-STEP_LOGIN_FILL_USER = STEP_READ_EMAIL_LOGIN + 1
-STEP_LOGIN_FILL_PASS = STEP_LOGIN_FILL_USER + 1
-STEP_ADD_MAIL = STEP_LOGIN_FILL_PASS + 1
-STEP_REQUEST_FB_CODE = STEP_ADD_MAIL + 1
+STEP_LOGIN = STEP_READ_EMAIL_LOGIN + 1
+STEP_SELECT_SUBJECT = STEP_LOGIN + 1
+STEP_REQUEST_FB_CODE = STEP_SELECT_SUBJECT + 1
 STEP_FILL_EMAIL_RECOVER = STEP_REQUEST_FB_CODE + 1
 STEP_FIND = STEP_FILL_EMAIL_RECOVER + 1
 STEP_CLICK_CONTINUE = STEP_FIND + 1
@@ -45,7 +44,8 @@ if __name__ == "__main__":
             exit(0)
             
         if (step == STEP_OPEN_URL):
-            ret = browser.open_url("http://dangkytinchi.ictu.edu.vn/")
+            # ret = browser.open_url("http://dangkytinchi.ictu.edu.vn/")
+            ret = browser.open_url("http://quanlydaotao.tump.edu.vn/dhyd/login.aspx")
             if (ret == 1):
                 step = STEP_READ_EMAIL_LOGIN
 
@@ -54,74 +54,18 @@ if __name__ == "__main__":
                 login_user, login_pass = account[num_account_used].split("|")
                 print("insert_username: " + login_user)
                 print("insert_password: " + login_pass)
-                step = STEP_LOGIN_FILL_USER
+                step = STEP_LOGIN
 
-        elif (step == STEP_LOGIN_FILL_USER):
+        elif (step == STEP_LOGIN):
             login_user, login_pass = account[num_account_used].split("|")
             ret = browser.insert_username(login_user)
             ret = browser.insert_password(login_pass)
             if (ret):
                 print("succeess")
-                step = STEP_LOGIN_FILL_PASS
+                step = STEP_SELECT_SUBJECT
 
-        elif (step == STEP_LOGIN_FILL_PASS):
-            pass
-            # if (ret):
-            #     step = STEP_ADD_MAIL
-
-        elif (step == STEP_ADD_MAIL):
-            if (num_emails_add_in_use < number_emails_add):
-                new_user_name, misc = emails_add[num_emails_add_in_use].split("@")
-                # add sub mail
-                ret = browser.add_sub_mail(login_pass, new_user_name)
-                if (ret == 0):
-                    continue
-
-                if (ret == 2):
-                    num_emails_add_in_use += 1
-                    continue
-
-                if (ret == 1):
-                    step = step + 1
-                    email_recover = emails_add[num_emails_add_in_use]
-                    num_emails_add_in_use += 1
-
-        # Open a new tab
+        elif (step == STEP_SELECT_SUBJECT):
+            browser.select_subject()
+            step = step + 1
         elif (step == STEP_REQUEST_FB_CODE):
-            ret = browser.open_new_tab("https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0")
-            if (ret == 1):
-                step = step + 1
-
-        elif (step == STEP_FILL_EMAIL_RECOVER):
-            ret = browser.fill_email_recover(email_recover)
-            if (ret == 1):
-                step += 1
-
-        elif (step == STEP_FIND):
-            ret = browser.click_find()
-            if (ret == 1):
-                ret = browser.check_search_email()
-                if (ret == 1):
-                    step = STEP_CLOSE_TAB
-                else:
-                    step += 1
-            else:
-                step += 1
-
-        elif (step == STEP_CLICK_CONTINUE):
-            ret = browser.check_try_another_way()
-            if (ret == 1):
-                ret = browser.click_continue()
-                if (ret == 1):
-                    step += 1
-            else:
-                ret = browser.click_continue()
-                if (ret == 1):
-                    step += 1
-
-        elif (step == STEP_CLOSE_TAB):
-            num_account_used += 1
-            step = STEP_READ_EMAIL_LOGIN
-            browser.close_current_tab()
-            browser.back_to_login()
-
+            pass
